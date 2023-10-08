082d2179c9bba5e8287a1ba5af51ecf926e134d7
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-08 11:58:34 -0700

    %{name}

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,29 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * main - Prints the multiplication of two numbers, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: If the program receives two arguments - 0.
- *         If the program does not receive two arguments - 1.
- */
-int main(int argc, char *argv[])
+  *array_range - creates an array of integers.
+  *@min: minimum value.
+  *@max: maximum value.
+  *
+  *Return: pointer to newly created array.
+  *NULL if malloc fails.
+  *NULL if min > max.
+  */
+int *array_range(int min, int max)
 {
-	int num1, num2, prod;
+	int range, i;
+	int *p;
 
-	if (argc != 3)
+	range = 0;
+
+	if (min > max)
+		return (NULL);
+
+	range = ((max + 1) - min);
+
+	p = malloc(range * sizeof(int));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < range; i++)
 	{
-		printf("Error\n");
-		return (1);
+		*(p + i) = min + i;
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
-
-	return (0);
+	return (p);
 }
