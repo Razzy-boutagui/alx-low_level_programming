0339b173d4f7df346d56fb6f82e391cad516f5e4
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-14 10:53:24 -0700

    %{name}

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int Number = 0;
+
+	while (Number < 10)
+	{
+	putchar('Number');
+
+	Number++;
+	}
+	putchar('\n');
+	return (0);
+}
