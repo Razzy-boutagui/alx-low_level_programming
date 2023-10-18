97dbaae4e121cde8c59ce50893a43c3a299cc5ba
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-18 08:23:44 -0700

    %{name}

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,22 +9,22 @@
 int main(void)
 {
 char first = '0';
-char last = '9';
-char First = 'a';
-char Last = 'f';
+int i = 0;
+while (i < 10)
 
-while (first <= last)
-
+{
 putchar(first);
 first++;
-
+i++;
 }
-while (First <= Last)
+char First = 'a';
+i = 0;
+while (i < 6)
 
 {
 putchar(First);
 First++;
-
+i++;
 }
 putchar('\n');
 return (0);
