1f3ac7bd610ff4f68181b356f8f6129a1408c40a
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-18 08:28:50 -0700

    %{name}

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: always returns 0
- *
- */
 
 int main(void)
 {
-char first = '0';
-int i = 0;
-while (i < 10)
+	char first = '0';
+	char last = '9';
+	char First = 'a';
+	char Last = 'f';
 
+	while (first <= last)
 {
-putchar(first);
-first++;
-i++;
+	putchar(first);
+	first++;
 }
-char First = 'a';
-i = 0;
-while (i < 6)
 
+	while (First <= Last)
 {
-putchar(First);
-First++;
-i++;
+	putchar(First);
+	First++;
 }
-putchar('\n');
-return (0);
+
+	putchar('\n');
+
+return 0;
 }
