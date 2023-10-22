64791d9e86002239d3993f91c40f521cb0f8ca84
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-22 12:08:59 -0700

    3rd task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char first = 'a', last = 'z';
+	int min = 0;
+
+	while (min < 10)
+	{
+		while (first <= last)
+		{
+			_putchar(first);
+			first++;
+			min++;
+		}
+		_putchar('\n');
+	}
+}
