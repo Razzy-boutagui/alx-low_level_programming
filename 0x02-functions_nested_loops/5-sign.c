e3cf5bc6ec92140337991bfd3e79fee0eec5e9f5
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-23 11:06:55 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * @n: number to check
+ *
+ * Return: 1 if n > than 0 and return 0 if = to 0 and return -1 if < than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0' + 0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
