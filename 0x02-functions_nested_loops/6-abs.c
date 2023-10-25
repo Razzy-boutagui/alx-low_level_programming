ac1aa31370a5082b0937134e2cb4b78cc7c6f8ce
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-25 10:40:05 -0700

    s

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,19 @@
 #include"main.h"
 
-/***/
+/**
+ * _abs - entry point
+ *
+ * @n: parametre
+ *
+ * Return: return the value of n
+ *
+ */
 
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		n = ( n * -1);
+		n = (n * -1);
 	}
 	return (n);
 }
