18c9520e65a87111ef280c27aedcdfa6ef268387
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-25 10:37:43 -0700

    8th task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: parameter
+ *
+ * Return: last_digit
+ *
+ */
+
 int print_last_digit(int n)
 {
-    int last_digit;
-    
-    if (n < 0)
-        n = -n;
+	 int last_digit;
 
-    last_digit = n % 10;
-    _putchar('0' + last_digit);
-    return last_digit;
+	if (n < 0)
+		n = -n;
+
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
 
