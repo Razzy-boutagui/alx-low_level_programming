76883c285ae49be79c789fdfdddbfd0a6156b4e1
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-29 11:16:47 -0700

    ss

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,15 @@
 
 int print_last_digit(int n)
 {
-	 int last_digit;
+	int last_digit = n % 10;
 
-	if (n < 0)
+	if (last_digit < 0)
 	{
-		n = -n;
+		last_digit = -last_digit;
 	}
-	last_digit = n % 10;
+
 	_putchar('0' + last_digit);
-	
+
 		return (last_digit);
 }
 
