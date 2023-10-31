47a9bcaaf02e4a26964437644c1c290731c1490d
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-31 10:57:35 -0700

    ss

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,11 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (result == 0)
+			if (j == 0)
 			{
 				_putchar(result + '0');
 			}
-			else if (result <= 9 && result != 0)
+			else if (result <= 9 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
