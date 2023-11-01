018b3d4209f785e0990f98540ad0357956c07fc7
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-01 07:14:59 -0700

    ss

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * add - Entry point
+ *
+ * @c: first variable
+ *
+ * @b: seconde variable
+ *
+ * Return: always 0
+ *
+ */
+
+int add(int c, int b)
+{
+	int result;
+
+	result = c + b;
+
+
+	return (result);
+}
