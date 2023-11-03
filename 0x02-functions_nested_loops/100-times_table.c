f95db6dbafe9205c262aebd116caaefc1ab3683d
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-03 11:01:33 -0700

    ss

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,64 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_times_table - times table function
+ * print_times_table - Entry point
  *
- * @n: integer to return
+ * @n: parameter to change
  *
  */
 
 void print_times_table(int n)
 {
-	int prod, mult, num;
-
+	int i, j, result;
 	if (n <= 15 && n >= 0)
-
 	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar (48);
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
 
-				prod = num mult;
-				if (prod <= 9)
+		for (i = 0; i <= n; i++ )
+		{
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+			{
+				result = i * j;
+
+					_putchar(',');
 					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod >= 100)
+
+				if (result < 100 && result > 9 && j != n)
 				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10)
-					_putchar((prod 10) + 48);
-				_putchar((prod % 10) + 48);
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				if (result < 10 && j != n)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				if (result > 99 && j != n)
+				{
+					_putchar(result / 100+ '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				if (result <= 9 && j == n)
+				{
+					_putchar(result + '0');
+				}
+				if (result > 9 && j == n && result < 100)
+				{
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				if (result > 99 && j == n)
+				{
+				_putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
+				_putchar(result % 10 + '0');
+				}
 			}
 			_putchar('\n');
-
 		}
 	}
 }
