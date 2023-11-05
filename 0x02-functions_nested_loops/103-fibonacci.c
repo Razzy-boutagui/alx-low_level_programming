b067c42bc1ada979c91fae275c193abb6450353e
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-05 08:24:26 -0800

    ss

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,19 +13,16 @@ int main(void)
 	b = 1;
 	even = 0;
 
-if (even <= 4000000)
-{
 	for (i = 0 ; i <= 50; i++)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		if (c % 2 == 0)
+		if (c % 2 == 0 && even <= 4000000)
 		{
 			even = c + even;
 		}
 	}
-}
 	printf("%i\n", even);
 	return (0);
 }
