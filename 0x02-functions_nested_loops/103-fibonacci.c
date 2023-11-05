81b6e1ffc5b3d519c505dc1a4f18b50f67ff7d50
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-05 08:20:17 -0800

    ss

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - Calculat even fibo numbers
+ *
+ * Return: Alwayse return 0
+ */
+
+int main(void)
+{
+	int a, b, c, i, even;
+
+	a = 0;
+	b = 1;
+	even = 0;
+
+if (even <= 4000000)
+{
+	for (i = 0 ; i <= 50; i++)
+	{
+		c = a + b
+		a = b
+		b = c
+		if (c % 2 == 0)
+		{
+			even = c + even
+		}
+	}
+}
+	printf("%i\n", even)
+	return (0);
+}
