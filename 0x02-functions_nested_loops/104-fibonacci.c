19ff4ae43a4249ed34ebf76837ebf2c331211ee7
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-05 09:38:53 -0800

    ss

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int a = 0, b = 1, c, i;
-
+	double a = 0, b = 1, c;
+	int i;
 	for (i = 1; i <= 98; i++)
 	{
 		c = a + b;
-		printf("%i, ", c);
+		printf("%lf, ", c);
 		a = b;
 		b = c;
 	}
