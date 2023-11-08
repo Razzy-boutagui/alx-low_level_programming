6c048a7d4e73f8196dafe5423dbecf98ab88dbd8
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-08 14:47:44 -0800

    ss

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-	int i; /*this is the error we shoud declare char not int */
+	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10)
+/*	while (i < 10)
 	{
 		putchar(i);
 	}
-
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
