7efc1e685421024889ba25703e321baf48c99876
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-11 14:17:03 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * mul - mutplie two int
+ *
+ * @a: first digit
+ *
+ * @b: second degit
+ *
+ * Return: alwayse 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int multi, a, b;
+
+	multi = a * b;
+
+	return (multi);
+}
