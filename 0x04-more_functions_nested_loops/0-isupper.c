cc8c7b09db1373808c729708a323a7ff4b78511f
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-11 14:05:54 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _isupper - check if uppercase
+ *
+ * Return: 0 if lower and 1 if upper
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
