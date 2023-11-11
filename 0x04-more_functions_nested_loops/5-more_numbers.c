485fb6128b721f27bd912362219391798c929fa3
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-11 15:02:33 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * more_numbers - print from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i, ones, tens;
+
+	tens = _putchar(i / 10 + '0');
+	ones = _putchar(i + '0');
+	for (i = 0; i <= 14; i++)
+	{
+		if (i < 10)
+		{
+			ones;
+		}
+		else
+			tens;
+			ones;
+	}
+	_putchar('\n');
+}
+
+
