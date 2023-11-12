278ff9ea7864f6813410d43eaadcf15dfc606ef3
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-11 17:29:45 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - print fixx if mul of 3 and buzz if mul of 5
+ *
+ * Return: alwayse return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	return (0);
+}
