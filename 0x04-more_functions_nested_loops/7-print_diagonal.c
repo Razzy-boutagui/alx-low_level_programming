3cb97d3b4ab6fbc2e2c8d6f03020189b10b38ad1
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-11 16:25:05 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_diagonal - print diagonal ends with /
+ *
+ * @n: how much times we print the diagonal
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
