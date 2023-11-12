a6d9e1f451bc6e7706524fd17bcc72ec28e48388
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-11 21:10:18 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * print_number - print number
+ *
+ * @n: the number we wwant to print
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 999)
+	{
+		_putchar(n / 1000 + '0');
+	}
+	if (n > 99)
+	{
+		_putchar(n / 100 % 10 + '0');
+	}
+	if (n > 9)
+	{
+		_putchar(n / 10 % 10 + '0');
+	}
+		_putchar(n % 10 + '0');
+}
