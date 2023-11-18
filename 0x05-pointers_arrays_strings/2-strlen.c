6e01c32927195db253c5af06452c4da7e00f37e3
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-18 15:23:00 -0800

    arrays-pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _strlen - return lenght of string
+ *
+ * @s: the string we have
+ * Return: return the lenght
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
