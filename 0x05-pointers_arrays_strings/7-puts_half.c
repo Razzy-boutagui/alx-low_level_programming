bdf5e57ff45da383a2d89ac7e32e486bc7c97839
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-23 11:13:05 -0800

    arrays-pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include"main.h"
+#include"2-strlen.c"
+
+/**
+ * puts_half - print the seconde half
+ *
+ * @str: the string to split
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int x = _strlen(str);
+
+	if (x % 2 == 0)
+	{
+	for (i = x / 2; i < x; i++)
+	{
+
+		_putchar(str[i]);
+	}
+	}
+	else 
+	{
+		for (i = (x - 1) / 2; i < x; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
