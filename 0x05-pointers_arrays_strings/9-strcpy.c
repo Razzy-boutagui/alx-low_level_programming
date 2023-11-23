cefa09f6fa0aa5eb178e1ccacc202048f5446e63
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-11-23 12:41:27 -0800

    arrays-pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include"main.h"
+#include"2-strlen.c"
+/**
+ * _strcpy(char *dest, char *src) - copy function
+ *
+ * @dest: where to past the copy
+ *
+ * @src: the string to copy
+ *
+ * Return: the value of dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int t = _strlen(src);
+	char cp;
+
+	for (i = 0; i < t; i++)
+	{
+		cp = src[i];
+		dest[i] = cp;
+	}
+	return (dest);
+}
